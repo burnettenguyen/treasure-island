08207a5c0ad2249a01e6f9fad69385882b8deb7c
Burnette <burnettenguyen@gmail.com>
2020-12-06 21:13:00 +0000

    added lower()

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ print("Your mission is to find the treasure.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-crossroad = input(f"You're at a cross road. Where do you want to go? Type 'left' or 'right'.")
+crossroad = input(f"You're at a cross road. Where do you want to go? Type 'left' or 'right'.").lower()
 
 if crossroad == "left":
-	lake = input(f"You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for the boat. Type 'swim' to swim across.")
+	lake = input(f"You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for the boat. Type 'swim' to swim across.").lower()
 
 	if lake == "wait":
-		color = input(f"You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow,and one blue. Which color do you choose?")
+		color = input(f"You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow,and one blue. Which color do you choose?").lower()
 
 		if color == "yellow":
 			print("You Win!")
